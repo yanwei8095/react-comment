e2c966d2e456b30bfc2b06d2afa490e1e0055b67
yanwei8095 <yanwei2007095@qq.com>
2020-07-06 18:31:48 +0800

    初始化及实现评论静态页面

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+import AddComment from './component/add-comment';
+import CommentList from './component/comment-list';
+
+export default class App extends Component{
+	render(){
+		return (
+			<div>
+				<header className="site-header jumbotron">
+					<div className="container">
+						<div className="row">
+							<div className="col-xs-12">
+								<h1>请发表对React的评论</h1>
+							</div>
+						</div>
+					</div>
+				</header>
+				<div className="container">
+					<AddComment/>
+					<CommentList/>
+				</div>
+  </div>
+		)
+	}
+}
